b3f7115f6ef2b803148fbce4c716b704fdb8e9ed
cristine <christinekhong8@gmail.com>
2019-10-25 13:05:37 +0700

    Create Kota.java

---
+++ TSP-euclidean-GA/src/Kota.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Kota {
+    int x;
+    int y;
+    
+    public Kota(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public double distanceToOther(Kota kotaLain){
+        int xDistance = Math.abs(this.getX() - kotaLain.getX());
+        int yDistance = Math.abs(this.getY() - kotaLain.getY());
+        
+        double distance = Math.sqrt( (xDistance*xDistance) + (yDistance*yDistance));
+        return distance;
+    }
+}
